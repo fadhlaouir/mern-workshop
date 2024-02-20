7ecf0ac2804a5594c5ccb4d8593ba415128bdd71
fadhlaouir <raed.fadhlaoui@hotmail.com>
2024-02-20 11:15:51 +0100

    feat: Implement Delete, Update and get by Id :tada:

--- backend/src/controllers/WorkshopController.js
+++ backend/src/controllers/WorkshopController.js
@@ -48,6 +48,8 @@ const createWorkshop = async (req, res) => {
   await workshop
     .save()
     .then((data) => {
+      succuess: true;
+      message: "Workshop created successfully";
       res.send(data);
     })
     .catch((err) => {
@@ -59,9 +61,100 @@ const createWorkshop = async (req, res) => {
 };
 
 /**
+ * get a workshop from the database and send it to the client side
+ * @param {*} req request object from the client side
+ * @param {*} res response object from the server side
+ */
+const getWorkshopById = async (req, res) => {
+  try {
+    // get the id from the request parameters
+    const id = req.params.id;
+
+    // find the workshop by id
+    const workshop = await Workshop.findById(id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        workshop,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message:
+        err.message || "Some error occurred while retrieving the workshop.",
+    });
+  }
+};
+
+/**
+ * update a workshop in the database and send it to the client side
+ * @param {*} req request object from the client side
+ * @param {*} res response object from the server side
+ */
+const updateWorkshop = async (req, res) => {
+  try {
+    // get the id from the request parameters
+    const id = req.params.id;
+
+    // find the workshop by id and update it
+    const workshop = await Workshop.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        workshop,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message:
+        err.message || "Some error occurred while updating the workshop.",
+    });
+  }
+};
+
+/**
+ * delete a workshop from the database and send it to the client side
+ * @param {*} req request object from the client side
+ * @param {*} res response object from the server side
+ */
+const deleteWorkshop = async (req, res) => {
+  try {
+    // get the id from the request parameters
+    const id = req.params.id;
+
+    // find the workshop by id and delete it
+    const workshop = await Workshop.findById(id);
+
+    if (!workshop) {
+      return res.status(404).json({ message: "Workshop not found" });
+    }
+
+    await workshop.remove();
+
+    res.status(200).json({ status: "success", message: "Workshop deleted" });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message:
+        err.message || "Some error occurred while deleting the workshop.",
+    });
+  }
+};
+
+/**
  * export all the functions to be used in the routes
  */
 module.exports = {
-  createWorkshop,
   getAllWorkshops,
+  createWorkshop,
+  getWorkshopById,
+  updateWorkshop,
+  deleteWorkshop,
 };
--- backend/src/routes/WorkShopRoutes.js
+++ backend/src/routes/WorkShopRoutes.js
@@ -32,5 +32,36 @@ router.post("/workshop", workShopController.createWorkshop);
  */
 router.get("/workshops", workShopController.getAllWorkshops);
 
+/**
+ * @route : /api/v1/workshops/:id
+ * @method : GET (read)
+ * @description : get a specific workshop in the database and return it
+ * @access : public (no authentication required)
+ * @return : { workshop } if the request is successful and the workshop is retrieved from the database
+ * @error : { error } if the request is not successful or if there is an error in the database
+ */
+router.get("/workshops/:id", workShopController.getWorkshopById);
+
+/**
+ * @route : /api/v1/workshops/:id
+ * @method : PUT (update)
+ * @description : update a specific workshop in the database and return it
+ * @access : public (no authentication required)
+ * @body : { name, description, date} the name, description and date of the workshop
+ * @return : { workshop } if the request is successful and the workshop is updated in the database
+ * @error : { error } if the request is not successful or if there is an error in the database
+ */
+router.put("/workshops/:id", workShopController.updateWorkshop);
+
+/**
+ * @route : /api/v1/workshops/:id
+ * @method : DELETE (delete)
+ * @description : delete a specific workshop in the database
+ * @access : public (no authentication required)
+ * @return : { message } if the request is successful and the workshop is deleted from the database
+ * @error : { error } if the request is not successful or if there is an error in the database
+ */
+router.delete("/workshops/:id", workShopController.deleteWorkshop);
+
 // Export the router
 module.exports = router;
